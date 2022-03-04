85cf3872f8baf44bbc4892f4c09d95112d4b580c
ZetyrStudy <97560331+ZetyrStudy@users.noreply.github.com>
2022-03-03 21:38:54 -0300

    20220303_01

--- lists.php
+++ lists.php
@@ -4,6 +4,7 @@
 
 <h3>Índice:</h3>
 <ul>
+    <li><a href="https://www.w3schools.com/html/html_lists.asp" target="_blank">Listas no W3Schools</a></li>
     <li>Listas não ordenadas</li>
     <li>Listas ordenadas</li>
     <li>Listas de definição</li>
@@ -108,45 +109,60 @@
     <li>Crie uma lista definindo cada uma das tags usadas em listas HTML.</li>
 </ol>
 
-<h3>Alunos da turma</h3>
+<hr>
+
+<h3>Soluções dos exercícios</h3>
+
+<h4>Exercício 1</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
+
 <ul>
-    <li>Stefan</li>
-    <li>Luca</li>
-    <li>Wendel</li>
-    <li>Vinicius</li>
+    <li>Anderson</li>
+    <li>Caio</li>
+    <li>Daniel</li>
+    <li>Davi</li>
+    <li>David</li>
     <li>Erika</li>
+    <li>Gabriel</li>
+    <li>João</li>
+    <li>Luca</li>
+    <li>Lucas</li>
+    <li>Rodrigo</li>
+    <li>Stefan</li>
+    <li>Vinícius</li>
+    <li>Wendel</li>
 </ul>
 
-<h3>Tarefas a serem executadas</h3>
+<h4>Exercício 2</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
+
 <ol>
-    <li type="1">Ligue o computador</li>
-    <li type="1">Logue no GitDesktop</li>
-    <li type="1">Acesse o Meet</li>
-    <li type="1">Acesse o Drive</li>
-    <li type="1">Ligue o XAMPP</li>
+    <li>10:00 - Acordar</li>
+    <li>10:10 - Cochilar</li>
+    <li>10:50 - Acordar atrasado</li>
+    <li>10:55 - Escovar os dentes</li>
+    <li>11:00 - Nõ tomar banho porque está sem água</li>
+    <li>11:05 - Encher-se de perfume para disfarçar o cheiro</li>
+    <li>11:10 - Sair para trabalhar com fome porque se atrasou</li>
+    <li>12:00 - Comer porcaria no cmelô para matar a fome</li>
 </ol>
 
+<h4>Exercício 3</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
+
 <dl>
-    <dt><strong>ul</strong> &rarr; Unordered List.</dt>
-    <dd>
-        <p>Uma lista não ordenada é uma lista de itens marcadas com símbolos.</p>
-
-        <p>Uma lista não ordenada começa com a tag &ltul&gt e cada item da lista começa com a tag &ltli&gt.</p>
-    </dd>
-    <dt><strong>ol</strong> &rarr; Ordered List.</dt>
-    <dd>
-        <p>Uma lista ordenada é também uma lista de itens marcadas com números.</p>
-
-        <p>Uma lista ordenada começa com a tag &ltol&gt e cada item da lista começa com a tag &ltli&gt.</p>
-    </dd>
-    <dt><strong>dl</strong> &rarr; Definition list</dt>
-    <dd>
-        <p>Uma lista de definições não é uma lista de itens com termos e explicações dos mesmos.</p>
-
-        <p>Uma lista de definições começa com a tag &ltdl&gt. Cada termo da lista de definições começa com a tag &ltdt&gt. Cada definição da lista de definições começa com a tag &ltdd&gt.</p>
-    </dd>
-
+    <dt>UL</dt>
+    <dd>Cria uma lista não ordenada</dd>
+    <dt>OL</dt>
+    <dd>Cria uma lista ordenada</dd>
+    <dt>LI</dt>
+    <dd>Cria um item de lista ordenada ou não ordenada</dd>
+    <dt>DL</dt>
+    <dd>Cria uma lista de definições</dd>
+    <dt>DT</dt>
+    <dd>Cria um termo a ser definido</dd>
+    <dt>DD</dt>
+    <dd>Apresenta a definição do termo</dd>
 </dl>
-
 
 <?php require __DIR__ . "/_footer.php"; ?>
--- tables.php
+++ tables.php
@@ -1,280 +1,275 @@
 <?php require __DIR__ . "/_header.php"; ?>
 
 <style>
-    table {
-        font-size: 16px;
-        text-align: center;
-        background: lightblue;
-    }
-
-    .table,
+    /**
+     * Folhas de estilo somente para facilitar a visualização dos elementos das tabelas.
+     */
+    table,
     tr,
-    td,
-    th {
+    th,
+    td {
         border: 1px solid black;
-    }
-
-    #lista-brunch {
-        border: 1px dotted black;
-        background: lightgoldenrodyellow;
-        display: table;
-        padding-right: 15px;
-        ;
-    }
-
-    #table-header {
-        background: grey;
-        font-weight: 800;
-        font-size: 1.2rem;
-    }
-
-    td,
-    th {
-        padding: .5rem;
-    }
-
-    .calendario {
-        background: black;
-        color: white;
-        display: table;
-        padding: 15px;
-    }
-
-    #tab-cal {
-        background: black;
-    }
-
-    #outside {
-        opacity: 0.8;
-        color: grey;
-    }
-
-    #today {
-        background: blue;
-        border: 3px solid darkblue;
     }
 </style>
 
 <h2>Tabelas</h2>
 
-<p><strong>table</strong> &rarr; Cria uma tabela.</p>
-<p><strong>tr</strong> &rarr; table row &rarr; Cria uma linha na tabela.</p>
-<p><strong>th</strong> &rarr; table header &rarr; Cria uma célula de cabeçalho dentro da linha.</p>
-<p><strong>td</strong> &rarr; table data &rarr; Cria uma célula de dados dentro da linha.</p>
-<p><strong>colspan</strong> &rarr; Expande coluna &rarr; Esse atributo espande uma célula na mesma linha.</p>
+<h3>Referências:</h3>
+<ul>
+    <li><a href="https://www.w3schools.com/html/html_tables.asp" target="_blank">Tabelas no W3Schools</a></li>
+</ul>
 
-<div class="exercicio1">
-    <table>
+<ul>
+    <li><strong>table</strong> &rarr; Cria uma tabela.</li>
+    <li><strong>tr</strong> &rarr; table row &rarr; Cria uma linha na tabela.</li>
+    <li><strong>th</strong> &rarr; table header &rarr; Cria uma célula de cabeçalho dentro da linha.</li>
+    <li><strong>td</strong> &rarr; table data &rarr; Cria uma célula de dados dentro da linha.</li>
+    <li><strong>colspan</strong> &rarr; Expande coluna &rarr; Esse atributo espande uma célula na mesma linha.</li>
+    <li><strong>rowspan</strong> &rarr; Expande linha &rarr; Esse atributo espande uma célula para a linha seguinte.</li>
+</ul>
 
-        <tr id="table-header">
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Nascimento</th>
-        </tr>
+<h3>Exemplo simples</h3>
 
-        <tr>
-            <td>Joca da Silva</td>
-            <td>joca@silva.com</td>
-            <td>10/11/1980</td>
-        </tr>
+<table>
 
-        <tr>
-            <td>Setembrino Trocatapas</td>
-            <td>set@brino.com</td>
-            <td>23/12/1979</td>
-        </tr>
+    <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Nascimento</th>
+    </tr>
 
-        <tr>
-            <td>Marineuza Siriliano</td>
-            <td>mari@neuza.com</td>
-            <td>02/02/2000</td>
-        </tr>
+    <tr>
+        <td>Joca da Silva</td>
+        <td>joca@silva.com</td>
+        <td>10/11/1980</td>
+    </tr>
 
-        <tr>
-            <td>Edilineuza Carmisona</td>
-            <td>edir@carmo.com</td>
-            <td>10/04/1971</td>
-        </tr>
+    <tr>
+        <td>Setembrino Trocatapas</td>
+        <td>set@brino.com</td>
+        <td>23/12/1979</td>
+    </tr>
 
-    </table>
+    <tr>
+        <td>Marineuza Siriliano</td>
+        <td>mari@neuza.com</td>
+        <td>02/02/2000</td>
+    </tr>
 
-    <h3>Outra tabela</h3>
+    <tr>
+        <td>Edilineuza Carmisona</td>
+        <td>edir@carmo.com</td>
+        <td>10/04/1971</td>
+    </tr>
 
-    <table>
+</table>
 
-        <tr id="table-header">
-            <th>Produto</th>
-            <th>Preço</th>
-            <th>Seção</th>
-            <th>Subseção</th>
-        </tr>
+<h3>Colspan e Rowspan</h3>
 
-        <tr>
-            <td>Bicicleta</td>
-            <td>R$1000,00</td>
-            <td colspan="2">Veículos</td>
-        </tr>
+<table>
 
-        <tr>
-            <td>Bola</td>
-            <td>R$20,00</td>
-            <td>Esportes</td>
-            <td>Bolas</td>
-        </tr>
+    <tr>
+        <th>Produto</th>
+        <th>Preço</th>
+        <th>Seção</th>
+        <th>Subseção</th>
+    </tr>
 
-        <tr>
-            <td>Patins</td>
-            <td>R$500,00</td>
-            <td colspan="2" rowspan="2">Veículos</td>
-        </tr>
+    <tr>
+        <td>Bicicleta</td>
+        <td>R$1000,00</td>
+        <td colspan="2">Veículos</td>
+    </tr>
 
-        <tr>
-            <td>Patinete</td>
-            <td>R$700,00</td>
-        </tr>
+    <tr>
+        <td>Bola</td>
+        <td>R$20,00</td>
+        <td>Esportes</td>
+        <td>Bolas</td>
+    </tr>
 
-    </table>
+    <tr>
+        <td>Patins</td>
+        <td>R$500,00</td>
+        <td colspan="2" rowspan="2">Veículos</td>
+    </tr>
 
-    <!--
-    Exercício: Crie mais uma tabela simples representando dados em 4 colunas e 6 linhas.
--->
+    <tr>
+        <td>Patinete</td>
+        <td>R$700,00</td>
+    </tr>
 
-    <h3>Tabela de Estoque</h3>
+</table>
 
-    <table>
-        <tr id="table-header">
-            <th>Nome do Produto</th>
-            <th>Preço (Unidade)</th>
-            <th>Departamento</th>
-            <th>Quantidade</th>
-        </tr>
-        <tr>
-            <td>Camisa T-shirt</td>
-            <td>R$50,00</td>
-            <td>Moda</td>
-            <td>1200</td>
-        </tr>
-        <tr>
-            <td>Calça Jeans Old</td>
-            <td>R$70,00</td>
-            <td>Moda</td>
-            <td>600</td>
-        </tr>
-        <tr>
-            <td>Gravata Listra diagonal</td>
-            <td>R$15,00</td>
-            <td>Moda</td>
-            <td>2200</td>
-        </tr>
-        <tr>
-            <td>Sapato Social Society</td>
-            <td>R$120,00</td>
-            <td>Sapatos</td>
-            <td>400</td>
-        </tr>
-        <tr>
-            <td>Cinto Largo-H</td>
-            <td>R$30,00</td>
-            <td>Moda</td>
-            <td>2100</td>
-        </tr>
-        <td>Perfume Romantic 2034</td>
-        <td>R$110,00</td>
-        <td>Perfumaria</td>
-        <td>300</td>
-        </tr>
-    </table>
-</div>
+<h3>Exercícios</h3>
+<ol>
+    <li>Crie mais uma tabela simples representando dados em 4 colunas e 6 linhas.</li><br>
+    <li>Crie uma pequena lista de compras para o brunch das crianças.</li><br>
+    <li>Clique no relógio do Windows e reproduza o calendário do mês atual usando HTML.</li>
+</ol>
 
-<p><strong>Exercício 2:</strong> Crie uma pequena lista de compras para o brunch das crianças.</p>
+<hr>
 
-<h3>Lista do brunch:</h3>
+<h3>Soluções dos exercícios</h3>
 
-<div id="lista-brunch">
-    <ul id="brunch">
-        <li>Bacon</li>
-        <li>Ovos</li>
-        <li>Pão francês</li>
-        <li>Linguiça</li>
-        <li>Torrada</li>
-        <li>Geléia de Morango</li>
-    </ul>
-</div>
-<p><strong>Exercício 3:</strong> Clique no relógio do Windows e reproduza o calendário do mês atual usando HTML.</p>
+<h4>Exercício 1</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
 
+<table>
 
+    <!-- Primeira linha - Cabeçalho -->
+    <tr>
+        <th>Cabeçalho 1</th>
+        <th>Cabeçalho 2</th>
+        <th>Cabeçalho 3</th>
+        <th>Cabeçalho 4</th>
+    </tr>
 
-<div class="calendario">
-    <hr>
-    <h3>fevereiro de 2022</h3>
-    <table id="tab-cal">
-        <tr>
-            <th>D</th>
-            <th>S</th>
-            <th>T</th>
-            <th>Q</th>
-            <th>Q</th>
-            <th>S</th>
-            <th>S</th>
-        </tr>
-        <tr>
-            <td id="outside">30</td>
-            <td id="outside">31</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-        </tr>
-        <tr>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-        </tr>
-        <tr>
-            <td>13</td>
-            <td>14</td>
-            <td>15</td>
-            <td>16</td>
-            <td>17</td>
-            <td>18</td>
-            <td>19</td>
-        </tr>
-        <tr>
-            <td>20</td>
-            <td>21</td>
-            <td>22</td>
-            <td>23</td>
-            <td id="today">24</td>
-            <td>25</td>
-            <td>26</td>
-        </tr>
-        <tr>
-            <td>27</td>
-            <td>28</td>
-            <td id="outside">1</td>
-            <td id="outside">2</td>
-            <td id="outside">3</td>
-            <td id="outside">4</td>
-            <td id="outside">5</td>
-        </tr>
-        <tr>
-            <td id="outside">6</td>
-            <td id="outside">7</td>
-            <td id="outside">8</td>
-            <td id="outside">9</td>
-            <td id="outside">10</td>
-            <td id="outside">11</td>
-            <td id="outside">12</td>
-        </tr>
+    <!-- Linhs 2 à 6 - Dados -->
+    <tr>
+        <td>Linha 2 - Coluna 1</td>
+        <td>Linha 2 - Coluna 2</td>
+        <td>Linha 2 - Coluna 3</td>
+        <td>Linha 2 - Coluna 4</td>
+    </tr>
 
-    </table>
-    <hr>
-</div>
+    <tr>
+        <td>Linha 3 - Coluna 1</td>
+        <td>Linha 3 - Coluna 2</td>
+        <td>Linha 3 - Coluna 3</td>
+        <td>Linha 3 - Coluna 4</td>
+    </tr>
+
+    <tr>
+        <td>Linha 4 - Coluna 1</td>
+        <td>Linha 4 - Coluna 2</td>
+        <td>Linha 4 - Coluna 3</td>
+        <td>Linha 4 - Coluna 4</td>
+    </tr>
+
+    <tr>
+        <td>Linha 5 - Coluna 1</td>
+        <td>Linha 5 - Coluna 2</td>
+        <td>Linha 5 - Coluna 3</td>
+        <td>Linha 5 - Coluna 4</td>
+    </tr>
+
+    <tr>
+        <td>Linha 6 - Coluna 1</td>
+        <td>Linha 6 - Coluna 2</td>
+        <td>Linha 6 - Coluna 3</td>
+        <td>Linha 6 - Coluna 4</td>
+    </tr>
+
+</table>
+
+<h4>Exercício 2</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
+
+<table>
+
+    <tr>
+        <th>Item</th>
+        <th>Qtd</th>
+        <th>Preço</th>
+    </tr>
+
+    <tr>
+        <td>Pão</td>
+        <td>10</td>
+        <td>R$0,50</td>
+    </tr>
+
+    <tr>
+        <td>Leite</td>
+        <td>1</td>
+        <td>R$4,00</td>
+    </tr>
+
+    <tr>
+        <td>Ovos</td>
+        <td>5</td>
+        <td>R$0,35</td>
+    </tr>
+
+    <tr>
+        <td>Pipoca</td>
+        <td>5</td>
+        <td>R$2,00</td>
+    </tr>
+
+    <tr>
+        <td>Pirulito</td>
+        <td>10</td>
+        <td>R$1,00</td>
+    </tr>
+
+</table>
+
+<h4>Exercício 3</h4>
+<p><em>Este é um exemplo de solução, não a solução definitiva. Para ver a solução, exiba o código fonte desta página.</em></p>
+Março de 2022
+<table>
+
+    <tr>
+        <th>D</th>
+        <th>S</th>
+        <th>T</th>
+        <th>Q</th>
+        <th>Q</th>
+        <th>S</th>
+        <th>S</th>
+    </tr>
+
+    <tr>
+        <td colspan="2"></td>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+    </tr>
+
+    <tr>
+        <td>6</td>
+        <td>7</td>
+        <td>8</td>
+        <td><strong><em>9</em></strong></td>
+        <td>10</td>
+        <td>11</td>
+        <td>12</td>
+    </tr>
+
+    <tr>
+        <td>13</td>
+        <td>14</td>
+        <td>15</td>
+        <td>16</td>
+        <td>17</td>
+        <td>18</td>
+        <td>19</td>
+    </tr>
+
+    <tr>
+        <td>20</td>
+        <td>21</td>
+        <td>22</td>
+        <td>23</td>
+        <td>24</td>
+        <td>25</td>
+        <td>26</td>
+    </tr>
+
+    <tr>
+        <td>27</td>
+        <td>28</td>
+        <td>29</td>
+        <td>30</td>
+        <td>31</td>
+        <td colspan="2"></td>
+    </tr>
+
+</table>
 
 
 
